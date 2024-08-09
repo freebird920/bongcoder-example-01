5956efce767292f7c1c178dab2147bfcd20dc342
freebird_han <freebird_han@naver.com>
2024-08-10 07:06:21 +0900

    chore: Update homepage layout and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 export default function HomePage() {
   return (
     <>
-      <h1>봉코더의 홈페이지입니다.</h1>
-      <section>
-        <p>사진이 들어갈 자리입니다.</p>
-      </section>
-      <section>
+      <section className="border-2 rounded-md" >
+      <h1 className="font-bold">봉코더의 홈페이지입니다.</h1>
+        <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg5eHw6MFKUmkfySbkwtjvDTLSu7hyy4aG25eOXNEu3fWn9EAH3XGZKfAdHlROpWUAU2h-cJZZXZx_p4tmYr-Uj8rvD5f8cLUeq8mhdZFPdbiHZRc7rBOEe2pz76d3bF7YZyUFFHOHglckQ/s450/hikouki_kinai.png"/>
+
         <h2>내 소개</h2>
         <p>이름: 봉우리</p>
         <p>나이: 14세</p>
